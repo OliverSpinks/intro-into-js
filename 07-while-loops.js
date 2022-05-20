ad81e562c987978deeedc39ee80d5a88f4629155
Oliver Spinks <oliver-spinks@hotmail.com>
2022-05-20 14:10:47 +0100

    Finished while loops js tutorial

---
+++ 07-while-loops.js
@@ -0,0 +1,19 @@
+let password = "tomorrow";
+let userGuess = "";
+
+// while (userGuess !== password) {
+//    userGuess = prompt ("Please enter password");
+// }
+
+// alert("Login Successgul");
+
+// 5 * 4 * 3 * 2 *2
+let factorial = 1;
+let number = 5;
+let original = number;
+do {
+    factorial = factorial * number;
+    number --
+} while(number > 0);
+
+console.log(original + "facetorial is " + factorial);
